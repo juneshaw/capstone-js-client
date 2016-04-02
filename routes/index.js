5f57e758783659232cc7d070970721a2ea7cf209
June Shaw <june.shaw@me.com>
2016-04-02 14:18:12 -0600

    getting google api working prepping for gmail

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+require(dotenv).load();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'ConnectBot' });
 });
 
+router.get('/send', function(req, res, next) {
+  console.log('KEY: ', GOOGLE_API_KEY);
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyA34gNqYa9dpD_u4MCmrWUTi-zbQlSw0yA');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onload=function() {
+    console.log((xhr.responseText));
+  };
+  xhr.send(JSON.stringify({'longUrl': 'HTTP://WWW.GOOGLE.COM'
+  }));
+
+  // });
+})
+
+// var restRequest = gapi.client.urlshortener.url.get({
+//   'shortUrl': 'http://goo.gl/fbs5'
+// });
+// restRequest.execute(function(jsonResponse, rawResponse){
+//Handle response
 module.exports = router;
