ac5f29b212c8a73c3f33319b7a67857a86e7760a
June Shaw <june.shaw@me.com>
2016-04-03 13:25:22 -0600

    printout of body from twilio

--- routes/index.js
+++ routes/index.js
@@ -118,9 +118,9 @@ router.get('/twsend', function(req, res, next) {
 
 router.get('/receive', function(req, res, next) {
   console.log('Received a message from Twilio: ');
-  console.log('req: ', req);
-  console.log('res: ', res);
-  console.log('next: ', next);
+  console.log('req: ', req.body);
+  // console.log('res: ', res);
+  // console.log('next: ', next);
 });
 
 module.exports = router;
