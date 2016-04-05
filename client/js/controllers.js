e55579b003114bdd132c0415862e7bfe433e77a3
June Shaw <june.shaw@me.com>
2016-04-05 10:13:24 -0600

    background to splashf

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,7 +1,10 @@
 app.controller('MainController', ['$scope', function($scope){
-      $scope.test='HTML and Routes Working'
-    }])
+  $scope.test='HTML and Routes Working'
+}]);
 
-app.controller('GroupListController', ['$scope', function($scope){
-      $scope.test='HTML and Routes Working'
-    }])
+app.controller('GroupIndexController', ['$scope', GroupService, function($scope){
+  GroupService.getGroups().then(function(payload) {
+    $scope.group_collection = payload.data;
+  }, function(error) {
+  });
+}]);
