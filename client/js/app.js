06bf5d9f4b505676dbcb1e686d9774bd428971c0
June Shaw <june.shaw@me.com>
2016-04-05 10:03:34 -0600

    background to splash

--- client/js/app.js
+++ client/js/app.js
@@ -1,9 +1,13 @@
 var app = angular.module('clientApp', ['ngRoute'])
         app.config(function($routeProvider) {
             $routeProvider
-                .when('/', {
-                    templateUrl: './partials/index.html',
-                    controller: 'MainController'
-                })
+            .when('/', {
+                templateUrl: './partials/index.html',
+                controller: 'MainController'
+            })
+            .when('/groups', {
+                templateUrl: './partials/group.html',
+                controller: 'GroupListController'
+            })
                 .otherwise({redirectTo : '/'})
         })
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,3 +1,7 @@
 app.controller('MainController', ['$scope', function($scope){
       $scope.test='HTML and Routes Working'
     }])
+
+app.controller('GroupListController', ['$scope', function($scope){
+      $scope.test='HTML and Routes Working'
+    }])
