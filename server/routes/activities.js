760629a1bdd94d77e1a84dba1df75030e24fef72
June Shaw <june.shaw@me.com>
2016-04-06 15:18:56 -0600

    fixed the db call for activity

--- server/routes/activities.js
+++ server/routes/activities.js
@@ -96,7 +96,7 @@ router.get('/', function(req, res, next) {
 router.get('/:id', function(req, res, next) {
   console.log('made it to the activities show with param id of: ', req.params.id);
   db.activity(req.params.id).first().then(function(data) {
-    console.log('data': data);
+    console.log('data: ', data);
       res.send({payload:data});
   });
 });
