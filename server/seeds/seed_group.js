e4a23e09272473c530f752b49b702ee4c310ca72
June Shaw <june.shaw@me.com>
2016-04-05 22:13:40 -0600

    moving images to public

--- server/seeds/seed_group.js
+++ server/seeds/seed_group.js
@@ -9,19 +9,19 @@ exports.seed = function(knex, Promise) {
       title: 'CSU Buddies',
       host_id: 1,
       bio: "Friends from CSU.  This group originated from dorm suitemates and extended friends.  We went on spring break trips, went to away athletic functions, hung out together.  We reunite during CSU football season, but are trying to get together occasionally during the rest of the year.",
-      image_url: 'images/friends_dusk.jpg',
+      image_url: 'https://s3-us-west-1.amazonaws.com/connectbot/friends_dusk.jpg',
       preference_id: 1}),
     knex('group').insert({id: 2,
       title: 'Galvanize Cohort',
       host_id: 1,
       bio: "Galvanize Full Stack G18 Cohort.  We went through 6 months of intense web developer curriculum together.  Now that we are off to our own adventures, we want to keep the group together",
-      image_url: 'images/friends_street.jpg',
+      image_url: 'https://s3-us-west-1.amazonaws.com/connectbot/friends_street.jpg',
       preference_id: 2}),
     knex('group').insert({id: 3,
       title: 'Cousins in Denver',
       host_id: 1,
       bio: "These are cousins from the Murphy side of my family.  There are 6 of us that live in the Denver metro area.  Family reunions stopped a while back, but we want to catch up every now and then!",
-      image_url: 'images/friends_river.jpg',
+      image_url: 'https://s3-us-west-1.amazonaws.com/connectbot/friends_river.jpg',
       preference_id: 3})
   );
 };
