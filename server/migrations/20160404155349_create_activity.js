61e9ccc541b55449aa58dd6b027ffd3fddbe88e4
June Shaw <june.shaw@me.com>
2016-04-06 10:32:24 -0600

    added group id to activity table

--- server/migrations/20160404155349_create_activity.js
+++ server/migrations/20160404155349_create_activity.js
@@ -1,6 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('activity', function(table){
     table.increments('id');
+    table.integer('group_id');
     table.string('name');
     table.integer('custom_category_id');
     table.integer('category_id');
