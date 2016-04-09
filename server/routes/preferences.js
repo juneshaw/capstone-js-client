c68bd8b11fb0f87f5ab73f0b6b761b3fa850c6ae
June Shaw <june.shaw@me.com>
2016-04-09 16:57:01 -0600

    preference buttons

--- server/routes/preferences.js
+++ server/routes/preferences.js
@@ -9,31 +9,26 @@ dotenv.load();
 
 /* GET index page. */
 router.get('/', function(req, res, next) {
-  console.log('got to the right preference route');
   db.Preferences().then(function(data) {
     res.send({payload:data});
   })
 });
 
 router.get('/:id', function(req, res, next) {
-  console.log('got to preference id route');
   db.preference(req.params.id).first().then(function(data) {
-    console.log('preference data: ', data);
     res.send({payload:data});
   });
 });
 
 router.get('/:id/group', function(req, res, next) {
-  console.log('got to group preference route');
   db.preferenceGroup(req.params.id).then(function(data) {
-    console.log('preference group data: ', data);
     res.send({payload:data});
   });
 });
 
 router.post('/:id/edit', function(req, res, next) {
   console.log('got to post preference route with id and req.body', req.params.id, ' ', req.body);
-  db.updatePreference(req.params.id, req.body.data).then(function(data) {
+  db.updatePreference(req.params.id, req.body.json.data).then(function(data) {
     console.console.log('did the updateActivity', req.body);;
   })
 })
