70f9a45ea599b496e71519d9198c5459609b43a5
June Shaw <june.shaw@me.com>
2016-04-10 12:07:00 -0600

    pref page

--- server/src/db.js
+++ server/src/db.js
@@ -281,7 +281,7 @@ groupByPreference: function(preferenceId) {
 
 preferenceCategoryByPreference: function(preferenceId) {
   return (this.Preference_Categories().where('preference_id', preferenceId));
-}
+},
 
 preferenceCategories: function(preferenceId) {
   console.log('in preferenceCategories in db.js with preferenceId: ', preferenceId);
