4d527c72bbdcdd3daa91af491f96b8be478096a4
June Shaw <june.shaw@me.com>
2016-04-09 21:48:11 -0600

    tried markers on map

--- server/routes/preferences.js
+++ server/routes/preferences.js
@@ -30,15 +30,19 @@ router.post('/:id/edit', function(req, res, next) {
   console.log('got to post preference route with id and req.body', req.params.id, ' ', req.body.json);
   console.log('req.body: ', req.body);
   console.log('req.body.json: ', req.body.json);
+  var jsonObj = JSON.parse(req.body.json);
+  console.log('jsonObj!!!: ', jsonObj);
+  console.log('jsonObj.time: ', jsonObj.time);
+
   console.log('req.body.json.time', req.body.json["time"]);
   console.log('req.body.json["data"]: ', req.body.json["data"]);
   console.log('req.body.json.data: ', req.body.json.data);
   // db.updatePreference(req.params.id, {"time":"19:00:00","day":"Saturday","periodicity":"biweekly"}).then(function(data) {
   db.updatePreference(req.params.id,
-    {time: req.body.json.time,
-      day: req.body.json.day,
-      periodicity: req.body.periodicity}).then(function(data) {
-    console.log('did the updatePreference', req.body.json);;
+    {time: jsonObj.time,
+      day: jsonObj.json.day,
+      periodicity: jsonObj.periodicity}).then(function(data) {
+    console.log('did the updatePreference', jsonObj);;
   })
 })
 
