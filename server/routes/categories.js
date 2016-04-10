cc210b21be1fc6b0942098683d1dd4eb88397b3f
June Shaw <june.shaw@me.com>
2016-04-09 23:38:35 -0600

    categories

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -9,30 +9,15 @@ dotenv.load();
 
 /* GET index page. */
 router.get('/', function(req, res, next) {
-  db.Preferences().then(function(data) {
+  db.Categories().then(function(data) {
     res.send({payload:data});
   })
 });
 
 router.get('/:id', function(req, res, next) {
-  db.preference(req.params.id).first().then(function(data) {
+  db.category(req.params.id).first().then(function(data) {
     res.send({payload:data});
   });
 });
-
-router.get('/:id/group', function(req, res, next) {
-  db.preferenceGroup(req.params.id).then(function(data) {
-    res.send({payload:data});
-  });
-});
-
-router.post('/:id/edit', function(req, res, next) {
-  var jsonObj = JSON.parse(req.body.json);
-  db.updatePreference(req.params.id,
-    {time: jsonObj.time,
-      day: jsonObj.day,
-      periodicity: jsonObj.periodicity}).then(function(data) {
-  })
-})
 
 module.exports = router;
