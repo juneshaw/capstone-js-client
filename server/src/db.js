785ebecb81e522a74a126bc32ce6c45d1cc1d3e8
June Shaw <june.shaw@me.com>
2016-04-09 19:17:32 -0600

    preference buttons

--- server/src/db.js
+++ server/src/db.js
@@ -95,7 +95,7 @@ preference: function(id) {
 },
 
 updatePreference: function(id, preference) {
-  console.log('got to updatePreference', id, preference);
+  console.log('got to updatePreference with id: ', id, ' and pref of: ', preference);
   return(this.Preferences().where('id', id).first().update(preference));
 },
 
