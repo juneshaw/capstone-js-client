7f42c54b72501dd6d6169e49662e579392dd1673
June Shaw <june.shaw@me.com>
2016-04-09 19:24:52 -0600

    preference buttons

--- server/routes/preferences.js
+++ server/routes/preferences.js
@@ -27,8 +27,8 @@ router.get('/:id/group', function(req, res, next) {
 });
 
 router.post('/:id/edit', function(req, res, next) {
-  console.log('got to post preference route with id and req.body', req.params.id, ' ', req.body);
-  db.updatePreference(req.params.id, {"time":"1970-01-01T17:10:00.000Z","day":"Saturday","periodicity":"biweekly"}).then(function(data) {
+  console.log('got to post preference route with id and req.body', req.params.id, ' ', req.body.json);
+  db.updatePreference(req.params.id, {"time":"19:00:00","day":"Saturday","periodicity":"biweekly"}).then(function(data) {
   // db.updatePreference(req.params.id, req.body.json.data).then(function(data) {
     console.console.log('did the updateActivity', req.body);;
   })
