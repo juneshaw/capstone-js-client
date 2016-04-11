2bcb8a274835894a8d1825645d9d05ed73e725ce
June Shaw <june.shaw@me.com>
2016-04-11 12:28:59 -0600

    activity engine

--- server/src/engine.js
+++ server/src/engine.js
@@ -1,18 +1,23 @@
 var db = require('./db.js');
+var utilities = require('./utilities.js');
 
 module.exports =
 {
 createActivity: function () {
   // Get today's date to see what activities need to be built.
-  var date = new Date();
-  console.log('Today is: ', date);
+  var dateToday = new Date();
+  console.log('Today is: ', dateToday);
 
   // Get the Groups
   db.Groups().then(function(data) {
     console.log('data: ****', data);
+
+    // For each group, calculate the number of days left until the next activity date.
     data.forEach(function(group) {
       console.log('group date: ', group.next_activity_date);
-      dayDiff()
+      var dayDiff = utilities.dayDiff2(dateToday, group.next_activity_date);
+      console.log('***** dayDiff: ', dayDiff);
+      // console.log('!!!!! difference with subtraction: ', group.next_activity_date-date);
       // res.send(group.date);
     })
   })
--- server/src/utilities.js
+++ server/src/utilities.js
@@ -1,16 +1,39 @@
+module.exports =
+{
+parseDateDay: function (dateString) {
+  console.log('dateString: ', dateString);
+  var monthday = dateString.split(' ')
+  console.log('monthday: ',monthday);
+  // return new Date(monthday[2], monthday[0]-1, monthday[1]);
+  var date = new Date(monthday[3], monthday[1]-1, monthday[2]);
+  console.log('date: ', date);
+  return date;
+},
 
-function parseDateDay(dateString) {
-    var monthday = dateString.split('/')
-    console.log('monthday: ',monthday);
-    // return new Date(monthday[2], monthday[0]-1, monthday[1]);
-    var date = new Date(monthday[3], monthday[1]-1, monthday[2]);
-    console.log('date: ', date);
-    return date;
-}
+dayDiff: function (firstDate, secondDate) {
+  var firstDay = this.parseDateDay(firstDate);
+  var secondDay = this.parseDateDay(secondDate);
+  var daydif = Math.round((second-first)/(1000*60*60*24));
+  return daydif;
+},
 
-function dayDiff(firstDate, secondDate) {
-    var firstDay = parseDateDay(firstDate);
-    var secondDay = parseDateDay(secondDate);
-    var daydif = Math.round((second-first)/(1000*60*60*24));
-    return daydif;
+dayDiff2: function (firstDate, secondDate) {
+  // One day in milliseconds
+  var one_day=1000*60*60*24
+
+  // Calculate the difference in the dates in days
+  var diff = Math.ceil((secondDate.getTime()-firstDate.getTime())/(one_day));
+
+  console.log('diff is: ', diff);
+    // var christmas=new Date(today.getFullYear(), 11, 25) //Month is 0-11 in JavaScript
+    // if (today.getMonth()==11 && today.getDate()>25) //if Christmas has passed already
+    //     christmas.setFullYear(christmas.getFullYear()+1) //calculate next year's Christmas
+    // //Set 1 day in milliseconds
+    // var one_day=1000*60*60*24
+    //
+    // //Calculate difference btw the two dates, and convert to days
+    // document.write(Math.ceil((christmas.getTime()-today.getTime())/(one_day))+
+    // " days left until Christmas!")
+    return diff;
+  }
 }
