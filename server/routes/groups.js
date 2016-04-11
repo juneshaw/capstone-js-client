956d76ba965c5051b245990ead5ee46f79900c24
June Shaw <june.shaw@me.com>
2016-04-10 22:30:23 -0600

    gen activity

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -25,6 +25,7 @@ router.get('/preference/:id', function(req, res, next) {
 })
 
 router.get('/activitygen/:id', function(req, res, next) {
+  console.log('in activitygen');
   engine.createActivity(req.params.id, req.body.location, req.body.preference).then(function(data) {
     console.log('engine activity: ',data);
     res.send(data);
